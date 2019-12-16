5799a6c6d54f5c6aafb4f14a7f0d3c3e7141bb95
Mark Leo <ratrateroo@gmail.com>
2019-12-17 03:41:42 +0800

    added user list page components

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './UserItem.css';
+const UserItem = () => {
+    
+}
+ 
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
 const Users = () => {
-    return <h2>Users Works!</h2>;
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Mark',
+            image: 'https://images.pexels.com/photos/6322/cold-snow-nature-winter.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            places: 3
+        },
+        {
+            id: 'u2',
+            name: 'Leo',
+            image: 'https://images.pexels.com/photos/1708601/pexels-photo-1708601.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            places: 2
+        }
+    ];
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
