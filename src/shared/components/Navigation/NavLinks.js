aedc0f849df4b7a5db2bb78e5814de87bd1d74fa
Mark Leo <ratrateroo@gmail.com>
2019-12-18 21:57:38 +0800

    added logout button and managed other buttons

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -7,26 +7,35 @@ import './NavLinks.css';
 const NavLinks = props => {
   const auth = useContext(AuthContext);
 
-  return <ul className="nav-links">
-    <li>
-      <NavLink to="/" exact>ALL USERS</NavLink>
-    </li>
-    {auth.isLoggedIn && (
+  return (
+    <ul className="nav-links">
       <li>
-        <NavLink to="/u1/places">MY PLACES</NavLink>
+        <NavLink to="/" exact>
+          ALL USERS
+        </NavLink>
       </li>
-    )}
-    {auth.isLoggedIn && (
-      <li>
-        <NavLink to="/places/new">ADD PLACE</NavLink>
-      </li>
-     )}
-    {!auth.isLoggedIn && (
-      <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
-      </li>
-    )}
-  </ul>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
+      )}
+    </ul>
+  );
 };
 
 export default NavLinks;
